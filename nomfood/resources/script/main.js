3f77874bdc6a83aa13732ecb93d4c33ed6356fd0
Aditya Gupta <iadityapansari@gmail.com>
2019-08-23 23:09:27 +0530

    adds animation.css

--- nomfood/resources/script/main.js
+++ nomfood/resources/script/main.js
@@ -22,4 +22,23 @@ $(document).ready(function () {
         $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 0);
     })
 
+
+    //Animation on scroll
+    //js--wp-1
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, { offset: '50%' })
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, { offset: '50%' })
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, { offset: '50%' })
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, { offset: '50%' })
+
 });
