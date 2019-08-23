f61e4788715f91dfef53267153926057b2568d88
Aditya Gupta <iadityapansari@gmail.com>
2019-08-23 20:34:38 +0530

    scrolling

--- nomfood/resources/script/main.js
+++ nomfood/resources/script/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+
+    // For sticky navigation
     $('.js--section-features').waypoint(function (direction) {
         if (direction == 'down') {
             //display sticky nav
@@ -9,5 +11,15 @@ $(document).ready(function () {
             $('nav').removeClass('sticky');
         }
     }, { offset: '60px' });
+
+
+    // For scrolling sections
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 0);
+    })
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 0);
+    })
 
 });
